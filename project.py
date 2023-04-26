48ad96c5ad6aa25818a0f8fe526a7fb0eb5eecb5
David <davidgonzalez1@csumb.edu>
2023-04-26 15:25:40 -0700

    setup

--- project.py
+++ project.py
@@ -2,4 +2,17 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+
+@app.route('/')
+def index():
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)   
