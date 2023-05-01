2f32527b832b519ec77223aaa0ccb889992fcc28
ArturoC <accesareo@gmail.com>
2023-04-30 21:06:04 -0700

    Testing if my Git is connected, no major changes

--- project.py
+++ project.py
@@ -5,19 +5,9 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
 @app.route('/')
 def index():
-
-
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-@app.route('/')
-def home():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)   
